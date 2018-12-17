a865a7b62109c7f35e086eba041e7dc302974d26
david2611 <d20.hall@qut.edu.au>
2018-12-17 14:20:33 +1000

    fixed issue with download scripts (not downloading zips with correct format names)

--- download_test_data.sh
+++ download_test_data.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 mkdir test_data
 cd test_data
-wget https://cloudstor.aarnet.edu.au/plus/s/C0KkMvqtNWgOzEi
-wget https://cloudstor.aarnet.edu.au/plus/s/tg5f9fk29uEjwiF
-wget https://cloudstor.aarnet.edu.au/plus/s/D8ABZoKZmCjuxBG
-wget https://cloudstor.aarnet.edu.au/plus/s/uFVFjlwK47VmGKI
-wget https://cloudstor.aarnet.edu.au/plus/s/Ti9cvjWSKQPwiJm
-wget https://cloudstor.aarnet.edu.au/plus/s/n5GBMzq0zU0jhNE
-wget https://cloudstor.aarnet.edu.au/plus/s/AvaGSVPxhCfsRFl
-wget https://cloudstor.aarnet.edu.au/plus/s/S4oNRTJS97rCIe1
-wget https://cloudstor.aarnet.edu.au/plus/s/1Nv26lcfLBWWI4f
-wget https://cloudstor.aarnet.edu.au/plus/s/qKyV1q8PgStXc1h
-wget https://cloudstor.aarnet.edu.au/plus/s/3TxUIRPTCPwLk3L
-wget https://cloudstor.aarnet.edu.au/plus/s/weIwMOvd5LuzMtS
-wget https://cloudstor.aarnet.edu.au/plus/s/1GmS6zLVp9pXadQ
-wget https://cloudstor.aarnet.edu.au/plus/s/MOsaXfIRpPXdLJI
-wget https://cloudstor.aarnet.edu.au/plus/s/pECoBogbbpWqgo6
-wget https://cloudstor.aarnet.edu.au/plus/s/VKRi9WPefaHUMQr
-wget https://cloudstor.aarnet.edu.au/plus/s/4u163tdszkI7lpT
-wget https://cloudstor.aarnet.edu.au/plus/s/dJ1SxOvQ0Ne3AzP
-unzip test_data_*.zip
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/C0KkMvqtNWgOzEi/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/tg5f9fk29uEjwiF/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/D8ABZoKZmCjuxBG/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/uFVFjlwK47VmGKI/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/Ti9cvjWSKQPwiJm/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/n5GBMzq0zU0jhNE/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/AvaGSVPxhCfsRFl/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/S4oNRTJS97rCIe1/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/1Nv26lcfLBWWI4f/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/qKyV1q8PgStXc1h/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/3TxUIRPTCPwLk3L/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/weIwMOvd5LuzMtS/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/1GmS6zLVp9pXadQ/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/MOsaXfIRpPXdLJI/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/pECoBogbbpWqgo6/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/VKRi9WPefaHUMQr/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/4u163tdszkI7lpT/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/dJ1SxOvQ0Ne3AzP/download
+unzip test_data_\*.zip
--- download_training_data.sh
+++ download_training_data.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 mkdir training_data
 cd training_data
-wget https://cloudstor.aarnet.edu.au/plus/s/Y2ibTrkl2tGUusf
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/Y2ibTrkl2tGUusf/download
 unzip training_data.zip
