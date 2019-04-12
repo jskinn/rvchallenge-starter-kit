49b09122366eead012c49391c0a9f4840fbe0fb5
david2611 <d20.hall@qut.edu.au>
2019-04-12 11:28:22 +1000

    fixed small error in download_test_dev_data.sh

--- download_test_dev_data.sh
+++ download_test_dev_data.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 mkdir test_dev
 cd test_dev
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/WRnFSo1Ya1t4Gjm
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/bJjxPNAhxy36sdy
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/Mnf3EHI4MjSmg0A
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/hQxiNcSDMOaIdFn
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/7TcgVJ49YyHuZsK
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/VD57gcDWkiSnut6
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/7wxIvt8YZQ90x4c
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/NGCpHbz7z7pkkAn
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/YKkhkIovlPwpN2A
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/ZMnAs1VqZQ9pcJ1
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/HUbxa72BWGKoEsG
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/MB4uzs9mHnchHql
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/BWMTnOpdOGy8XTp
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/4eLNxtQ4VenJVol
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/r7q4P4h6TWYLxUW
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/SpCCbekeJ7ZOwOL
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/KcBmNlpWeLc6xat
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/xNTjsANqWZZFRGB
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/WRnFSo1Ya1t4Gjm/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/bJjxPNAhxy36sdy/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/Mnf3EHI4MjSmg0A/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/hQxiNcSDMOaIdFn/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/7TcgVJ49YyHuZsK/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/VD57gcDWkiSnut6/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/7wxIvt8YZQ90x4c/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/NGCpHbz7z7pkkAn/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/YKkhkIovlPwpN2A/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/ZMnAs1VqZQ9pcJ1/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/HUbxa72BWGKoEsG/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/MB4uzs9mHnchHql/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/BWMTnOpdOGy8XTp/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/4eLNxtQ4VenJVol/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/r7q4P4h6TWYLxUW/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/SpCCbekeJ7ZOwOL/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/KcBmNlpWeLc6xat/download
+wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/xNTjsANqWZZFRGB/download
 unzip test_dev\*.zip
