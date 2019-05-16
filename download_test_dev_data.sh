86f33d33040f0a143572ca2d6c99045d6e2e8575
david2611 <d20.hall@qut.edu.au>
2019-05-16 14:52:25 +1000

    Updated download_test_dev_data.sh to fix links from researchdatafinder

--- download_test_dev_data.sh
+++ download_test_dev_data.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 mkdir test_dev
 cd test_dev
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/WRnFSo1Ya1t4Gjm/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/bJjxPNAhxy36sdy/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/Mnf3EHI4MjSmg0A/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/hQxiNcSDMOaIdFn/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/7TcgVJ49YyHuZsK/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/VD57gcDWkiSnut6/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/7wxIvt8YZQ90x4c/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/NGCpHbz7z7pkkAn/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/YKkhkIovlPwpN2A/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/ZMnAs1VqZQ9pcJ1/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/HUbxa72BWGKoEsG/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/MB4uzs9mHnchHql/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/BWMTnOpdOGy8XTp/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/4eLNxtQ4VenJVol/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/r7q4P4h6TWYLxUW/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/SpCCbekeJ7ZOwOL/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/KcBmNlpWeLc6xat/download
-wget --content-disposition https://cloudstor.aarnet.edu.au/plus/s/xNTjsANqWZZFRGB/download
-unzip test_dev\*.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/52e1b3ed-5ee8-4258-aa43-b790ccfe7e9a/download/testdev000000.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/d8b7570b-2544-4747-9a73-4562775726c1/download/testdev000001.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/ed8d0515-cb0f-4653-83b1-c535565b5302/download/testdev000002.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/67b4f6e0-1b63-499a-be69-747c8ae812aa/download/testdev000003.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/506d34dc-782b-44e8-9ce3-2f45470c3b8a/download/testdev000004.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/778a9811-e156-47b3-8851-9d87e128ef5f/download/testdev000005.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/47d1c8db-b23c-40c9-96e5-2856b49c244e/download/testdev000006.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/5fa620de-63e7-4e79-be26-a264151a5ccc/download/testdev000007.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/9ae3ed00-1b16-4682-8a4b-c5ed28ef4108/download/testdev000008.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/dd091173-df6f-424e-8db0-74b0526d00ef/download/testdev000009.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/2efc5756-e9a2-4820-8ee5-69dadd0dc1b7/download/testdev000010.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/ce91ddd0-fbc1-4308-a618-89cd7eb35f13/download/testdev000011.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/0d943453-be21-4631-acdc-308c06dff4c7/download/testdev000012.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/d2e29bf3-2fec-4072-803d-3cb02b1e03e6/download/testdev000013.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/d7a0693a-568b-4d77-ae62-7f305cf95e67/download/testdev000014.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/69137ce9-d0f5-4294-951e-42fbcfa35de6/download/testdev000015.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/8e77c2b7-c9d0-4e35-b892-ac6e8f7ef154/download/testdev000016.zip
+wget --content-disposition https://data.researchdatafinder.qut.edu.au/dataset/f3e2d808-e211-4a63-92d6-7bb79bb6e4ad/resource/dcf90744-f60e-4a4e-a9b7-5fe19f26d194/download/testdev000017.zip
+unzip testdev\*.zip
